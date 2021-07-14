d48f05060612d8dede0319e7ba21f5795c824a5b
yeonii98 <ajy7424@naver.com>
2021-07-14 17:50:07 +0900

    boj 10162

---
+++ BOJ/10162.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class Main{
+    public static void main(String[] args) {
+        int[] numArray = {300, 60, 10};
+        Scanner scanner = new Scanner(System.in);
+        int T = scanner.nextInt();
+
+        if(T % 10 == 0) {
+            for(int i = 0; i < 3; i++){
+                if(T % numArray[i] < T) {
+                    int temp = numArray[i];
+                    numArray[i] = T / numArray[i];
+                    T = T % temp;
+                }
+                else numArray[i] = 0;
+            }
+            System.out.print(numArray[0] + " " + numArray[1] + " " + numArray[2]);
+        }
+        else
+            System.out.println(-1);
+    }
+}
