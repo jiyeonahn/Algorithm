4939456c920662fe573bee5cf4d41742b2c7dc94
yeonii98 <ajy7424@naver.com>
2021-08-02 17:09:32 +0900

    Update 가장 먼 노드.java

--- Programmers/Level3/가장 먼 노드.java
+++ Programmers/Level3/가장 먼 노드.java
@@ -1,12 +1,12 @@
 import java.util.*;
 class Solution {
     public int solution(int n, int[][] edge) {
-        boolean[][] visit = new boolean[n + 1][n + 1];
+        boolean[][] graph = new boolean[n + 1][n + 1];
         int[] distance = new int[n+1];
         int node = 0;
 
         for(int i = 0; i < edge.length; i++)
-            visit[edge[i][0]][edge[i][1]] = visit[edge[i][1]][edge[i][0]] = true;
+            graph[edge[i][0]][edge[i][1]] = graph[edge[i][1]][edge[i][0]] = true;
 
         Queue<Integer> queue = new LinkedList<Integer>();
         queue.offer(1);
@@ -14,7 +14,7 @@ class Solution {
         while(!queue.isEmpty()){
             node = queue.poll();
             for(int i = 2; i <= n; i++){
-                if(distance[i] == 0 && visit[node][i] == true){
+                if(distance[i] == 0 && graph[node][i] == true){
                     queue.offer(i);
                     distance[i] = distance[node] + 1;
                 }
