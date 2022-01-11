c4eca0e93e4aea33dc29ff2a6572faa0a451bae8
yeonii98 <ajy7424@naver.com>
2022-01-11 23:46:59 +0900

    boj 20220111

---
+++ BOJ/boj6064.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class boj6064 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());//이동해야 할 채널
+        for(int i = 0; i < T; i++){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int M = Integer.parseInt(st.nextToken());
+            int N = Integer.parseInt(st.nextToken());
+            int X = Integer.parseInt(st.nextToken());
+            int Y = Integer.parseInt(st.nextToken());
+
+        }
+        int answer;
+
+    }
+}
