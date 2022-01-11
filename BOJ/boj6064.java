8282dba3d17f42457ad52c892cd9182c7ffa2656
yeonii98 <ajy7424@naver.com>
2022-01-12 02:50:59 +0900

    boj 20220111

--- BOJ/boj6064.java
+++ BOJ/boj6064.java
@@ -1,21 +1,40 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.StringTokenizer;
 
-public class boj6064 {
+public class boj6064 {//카잉달력
+    public static int gcd(int a, int b){
+        if(a == 0)
+            return b;
+        return gcd(b % a, a);
+    }
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int T = Integer.parseInt(br.readLine());//이동해야 할 채널
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int T = Integer.parseInt(br.readLine());
         for(int i = 0; i < T; i++){
             StringTokenizer st = new StringTokenizer(br.readLine());
             int M = Integer.parseInt(st.nextToken());
             int N = Integer.parseInt(st.nextToken());
-            int X = Integer.parseInt(st.nextToken());
-            int Y = Integer.parseInt(st.nextToken());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
 
+            int lcm = M * N * gcd(M,N);//멸망년도
+
+            int k = x;
+            y = y%N;//N은 항상 y보다 크다. y==N인경우 k%N은 0이 나올 수밖에 없다.
+            while(k <= lcm){
+                if(k%N == y){
+                    break;
+                }
+                k += M;
+            }
+            if(k > lcm)
+                bw.write(-1 + "\n");
+            else
+                bw.write(k + "\n");
         }
-        int answer;
-
+        bw.flush();
+        bw.flush();
     }
 }
