8c9480b0e4c7476907a61d76c7b0b2060120ad2b
yeonii98 <ajy7424@naver.com>
2022-02-12 01:44:14 +0900

    boj 20220211

--- BOJ/boj1167.java
+++ BOJ/boj1167.java
@@ -1,66 +1,67 @@
 import java.util.*;
 import java.io.*;
 
-public class boj1167 {
-    static ArrayList<Node>[] list;
-    static boolean[] visit;
-    static int max = 0;
-    static int node, V;
+public class boj1167 {//트리의 지름
+    //가장 긴 지름을 만드는 노드가 node1, node2 라면
+    //임의의 노드 1개에서 가장 먼 노드는 node1이나 node2 이다.
+    //임의의 점에서 가장 먼 거리의 노드를 찾고(node1), node1에서 가장 먼 거리의 노드(node2)를 찾는다.
+    //총 두번의 dfs를 돌리면 결과가 나오는 문제, 모든 노드를 돌릴 필요 x
 
-    public static void main(String[] args) throws Exception {
+    private static ArrayList<Node>[] list;
+    private static int answer, node;
+    private static boolean[] visited;
+
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
         StringTokenizer st;
 
-        V = Integer.parseInt(br.readLine());
-
-        list = new ArrayList[V + 1];
-        for (int i = 1; i <= V; i++)
+        visited = new boolean[N];
+        list = new ArrayList[N];
+        for (int i = 0; i < N; i++) {
             list[i] = new ArrayList<>();
+        }
 
-        for (int i = 0; i < V; i++) {
+        for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
-
-            int nodenum = Integer.parseInt(st.nextToken());
-            String str;
-            while (!(str = st.nextToken()).equals("-1")) {
-                int node = Integer.parseInt(str);
-                int dist = Integer.parseInt(st.nextToken());
-                list[nodenum].add(new Node(node, dist));
+            int n = Integer.parseInt(st.nextToken()) - 1;
+            while (st.hasMoreTokens()) {
+                int l = Integer.parseInt(st.nextToken()) - 1;
+                if (l == -2) break;
+                int d = Integer.parseInt(st.nextToken());
+                list[n].add(new Node(l, d));
             }
         }
 
-        visit = new boolean[V + 1];
-        dfs(1, 0);
+        visited[0] = true;
+        dfs(0, 0);
+        Arrays.fill(visited,false);
 
-        visit = new boolean[V + 1];
+        visited[node] = true;
         dfs(node, 0);
-
-        System.out.println(max);
-
+        System.out.println(answer);
     }
 
-    public static void dfs(int v, int len) {
-        if (len > max) {
-            max = len;
-            node = v;
+    static void dfs(int start, int sum) {
+        if(sum > answer){
+            answer = sum;
+            node = start;
         }
-
-        visit[v] = true;
-
-        for (Node n : list[v]) {
-            if (!visit[n.node]) {
-                dfs(n.node, n.dist + len);
-                visit[n.node] = true;
+        for (Node i : list[start]) {
+            if (!visited[i.link]) {
+                visited[i.link] = true;
+                dfs(i.link, sum + i.distance);
             }
         }
     }
 
     static class Node {
-        int node, dist;
+        int link;
+        int distance;
 
-        public Node(int node, int dist) {
-            this.node = node;
-            this.dist = dist;
+        public Node(int link, int distance) {
+            this.link = link;
+            this.distance = distance;
         }
     }
 }
