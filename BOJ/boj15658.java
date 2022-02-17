9b11a36e417d126e920284d77742238b3d25eb4b
yeonii98 <ajy7424@naver.com>
2022-02-17 16:32:46 +0900

    boj 20220216

--- BOJ/boj15658.java
+++ BOJ/boj15658.java
@@ -3,62 +3,58 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class boj15658 {
-    static int n;
-    static int[] nums;
-    static int[] operator;
-    static int result_min = Integer.MAX_VALUE;
-    static int result_max = Integer.MIN_VALUE;
+public class boj15658 { // 연산자 끼워넣기 (2)
+    static int N, max = -1000000000, min = 1000000000;
+    static int[] num;
+    static int[] op;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        n = Integer.parseInt(br.readLine());
-        nums = new int[n];
+        N = Integer.parseInt(br.readLine());
+        num = new int[N];
 
         StringTokenizer st = new StringTokenizer(br.readLine());
-        for(int i=0; i<n; i++){
-            nums[i] = Integer.parseInt(st.nextToken());
+        for(int i = 0; i < N; i++){
+            num[i] = Integer.parseInt(st.nextToken());
         }
 
-        operator = new int[4];
+        op = new int[4];
         st = new StringTokenizer(br.readLine());
-        for(int i=0; i<4; i++){
-            operator[i] = Integer.parseInt(st.nextToken());
+        for(int i = 0; i < 4; i++){
+            op[i] = Integer.parseInt(st.nextToken());
         }
 
-        dfs(1, nums[0]);
-
-        System.out.println(result_max);
-        System.out.println(result_min);
+        dfs(num[0], 0);
+        System.out.println(max);
+        System.out.println(min);
     }
 
-    public static void dfs(int idx, int sum){
-        if(idx==n){
-            result_min = Math.min(result_min, sum);
-            result_max = Math.max(result_max, sum);
+    private static void dfs(int result, int idx){
+        if(idx == N-1){
+            max = Math.max(max, result);
+            min = Math.min(min, result);
             return;
         }
-
-        for(int i=0; i<4; i++){
-            if(operator[i] == 0) continue;
-            operator[i]--;
-            switch (i){
-                case 0:
-                    dfs(idx + 1, sum + nums[idx]);
-                    break;
-                case 1:
-                    dfs(idx + 1, sum - nums[idx]);
-                    break;
-                case 2:
-                    dfs(idx + 1, sum * nums[idx]);
-                    break;
-                case 3:
-                    dfs(idx + 1, sum / nums[idx]);
-                    break;
+        for(int i = 0; i < 4; i++){
+            if(op[i] > 0){
+                op[i]--;
+                switch (i){
+                    case 0:
+                        dfs(result + num[idx + 1], idx + 1);
+                        break;
+                    case 1:
+                        dfs(result - num[idx + 1], idx + 1);
+                        break;
+                    case 2:
+                        dfs(result * num[idx + 1], idx + 1);
+                        break;
+                    case 3:
+                        dfs(result / num[idx + 1], idx + 1);
+                        break;
+                }
+                op[i]++;
             }
-            operator[i]++;
         }
-
     }
 
 }
