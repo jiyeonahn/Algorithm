6f0164af9bff4c8da72ecd1c37b865bf456260fa
JiYeon Ahn <76156034+yeonii98@users.noreply.github.com>
2024-07-01 15:00:38 +0900

    [level 0] Title: 짝수 홀수 개수, Time: 0.02 ms, Memory: 72.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120824. 짝수 홀수 개수/짝수 홀수 개수.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int[] solution(int[] num_list) {
+        int[] answer = new int[2];
+        for(int num : num_list){
+                answer[num % 2]++;
+            
+        }
+        return answer;
+    }
+}
