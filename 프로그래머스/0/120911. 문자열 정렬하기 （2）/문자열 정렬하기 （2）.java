7b18c63b3aa73dbec01c818024d20f93d24a58c6
JiYeon Ahn <76156034+yeonii98@users.noreply.github.com>
2024-07-02 21:38:44 +0900

    [level 0] Title: 문자열 정렬하기 (2), Time: 0.37 ms, Memory: 71.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/120911. 문자열 정렬하기 （2）/문자열 정렬하기 （2）.java
@@ -0,0 +1,10 @@
+import java.util.Arrays;
+class Solution {
+    public String solution(String my_string) {
+        char[] chArr = my_string.toLowerCase().toCharArray();
+
+        Arrays.sort(chArr);
+        
+        return new String(chArr);
+    }
+}
