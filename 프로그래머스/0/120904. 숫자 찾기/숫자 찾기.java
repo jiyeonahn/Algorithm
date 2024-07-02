5b615e4d860f7ff53689810b4f69515e4779fa40
JiYeon Ahn <76156034+yeonii98@users.noreply.github.com>
2024-07-02 21:23:38 +0900

    [level 0] Title: 숫자 찾기, Time: 7.30 ms, Memory: 74.2 MB -BaekjoonHub

--- 프로그래머스/0/120904. 숫자 찾기/숫자 찾기.java
+++ 프로그래머스/0/120904. 숫자 찾기/숫자 찾기.java
@@ -1,11 +1,6 @@
 class Solution {
     public int solution(int num, int k) {
-        String str = num + "";
-        for(int i = 1; i <= str.length(); i++){
-            if(Integer.parseInt(str.substring(i-1,i)) == k){
-                return i;
-            }
-        }
-        return -1;
+        
+        return ("-"+num).indexOf(k+"");
     }
 }
