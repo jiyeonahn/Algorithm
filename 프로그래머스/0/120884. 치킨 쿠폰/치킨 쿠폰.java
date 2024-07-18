d76baa59418aa16f62e9d326ffdbac00505c6f21
JiYeon Ahn <76156034+yeonii98@users.noreply.github.com>
2024-07-18 22:13:59 +0900

    [level 0] Title: 치킨 쿠폰, Time: 0.03 ms, Memory: 81.7 MB -BaekjoonHub

--- 프로그래머스/0/120884. 치킨 쿠폰/치킨 쿠폰.java
+++ 프로그래머스/0/120884. 치킨 쿠폰/치킨 쿠폰.java
@@ -1,14 +1,13 @@
 class Solution {
     public int solution(int chicken) {
         int answer = 0;
-        int coupon = 0;
-        while(chicken >= 10){
-            answer += chicken/10;
-            coupon += chicken%10;
-            chicken = chicken/10;
+        int coupon = chicken;
+        while (coupon >= 10) {
+            chicken = coupon / 10;
+            coupon %= 10;
+            coupon += chicken;
+            answer += chicken;
         }
-    
-        answer = answer + (chicken+coupon)/10 + ((chicken+coupon)/10+(chicken+coupon)%10)/10;
         return answer;
     }
 }
