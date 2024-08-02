b229cb04b56b0295a3f2d30339f4d917fb33dfcc
JiYeon Ahn <76156034+jiyeonahn@users.noreply.github.com>
2024-08-02 10:50:38 +0900

    [level 1] Title: 두 정수 사이의 합, Time: 10.30 ms, Memory: 74.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/12912. 두 정수 사이의 합/두 정수 사이의 합.java
@@ -0,0 +1,12 @@
+class Solution {
+    public long solution(int a, int b) {
+        long answer = 0;
+        if(a == b) return a;
+        int min = a < b ? a : b;
+        int max = min == a ? b : a;
+        for(long i = min; i <= max; i++){
+            answer += i;
+        }
+        return answer;
+    }
+}
