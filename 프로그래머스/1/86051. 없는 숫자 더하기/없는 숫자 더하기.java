34c7e1fa30edf04622556e5cdbe6d6fd5178a10d
JiYeon Ahn <76156034+jiyeonahn@users.noreply.github.com>
2024-08-07 10:29:06 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.03 ms, Memory: 75.9 MB -BaekjoonHub

--- 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.java
+++ 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.java
@@ -1,17 +1,10 @@
 import java.util.Arrays;
 class Solution {
     public int solution(int[] numbers) {
-        int answer = 0;
-        StringBuilder sb = new StringBuilder();
-        for(int n : numbers){
-            sb.append(n);
+        int answer = 45;
+        for(int num : numbers){
+            answer -= num;
         }
-        String numStr = sb.toString();
-        
-        for(int i = 1; i < 10; i++){
-            if(!numStr.contains(String.valueOf(i))) answer += i;
-        }
-        
         return answer;
     }
 }
