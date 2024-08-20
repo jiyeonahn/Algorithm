0424ce5775c299caeeb4f092ba8c95318913f6b0
JiYeon Ahn <76156034+jiyeonahn@users.noreply.github.com>
2024-08-20 10:38:47 +0900

    [level 1] Title: 시저 암호, Time: 0.84 ms, Memory: 76.7 MB -BaekjoonHub

--- 프로그래머스/1/12926. 시저 암호/시저 암호.java
+++ 프로그래머스/1/12926. 시저 암호/시저 암호.java
@@ -1,16 +1,16 @@
 class Solution {
     public String solution(String s, int n) {
-        String answer = "";
+        StringBuilder sb = new StringBuilder();
         for(char ch : s.toCharArray()){
             if(ch == ' '){
-                answer += ch;
+                sb.append(ch);
                 continue;
             } 
             if(ch+n > 122 || (ch >= 'A' && ch <= 'Z' && ch+n > 90)) {
-                answer += ((char)(ch+n-26));
+                sb.append((char)(ch+n-26));
             }
-            else answer += ((char)(ch+n));
+            else sb.append((char)(ch+n));
         }
-        return answer;
+        return sb.toString();
     }
 }
