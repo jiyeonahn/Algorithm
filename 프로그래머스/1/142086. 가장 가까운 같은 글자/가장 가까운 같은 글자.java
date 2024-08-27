5f4ac4a24f5f50f26c67fef17cb1c52cefb36414
JiYeon Ahn <76156034+jiyeonahn@users.noreply.github.com>
2024-08-27 10:35:16 +0900

    [level 1] Title: 가장 가까운 같은 글자, Time: 31.55 ms, Memory: 99.2 MB -BaekjoonHub

--- 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.java
+++ 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.java
@@ -1,9 +1,10 @@
 class Solution {
     public int[] solution(String s) {
         int[] answer = new int[s.length()];
+        int idx = 0;
         for(int i = 0; i < s.length(); i++){
-            int idx = s.substring(0,i).lastIndexOf(s.charAt(i));
-            answer[i] = idx == -1 ? -1 : s.substring(0,i).length() - idx;
+            idx = s.substring(0,i).lastIndexOf(s.charAt(i));
+            answer[i] = idx == -1 ? -1 : i - idx;
             
         }
         return answer;
