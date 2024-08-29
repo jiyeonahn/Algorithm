9872a5acd1a3080e01f16f9508de690a7fa6b96b
JiYeon Ahn <76156034+jiyeonahn@users.noreply.github.com>
2024-08-29 12:12:25 +0900

    [level 1] Title: 콜라 문제, Time: 0.13 ms, Memory: 75.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/132267. 콜라 문제/콜라 문제.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int solution(int a, int b, int n) {
+        int answer = 0;
+        int num = 0;
+        while(n > a-1){
+            answer += n/a * b;
+            n = n/a * b + n%a;
+        }
+        return answer;
+    }
+}
