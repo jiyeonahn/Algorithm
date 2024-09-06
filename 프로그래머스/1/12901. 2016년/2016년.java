3abdf27b2a4aba6c8f76e0f5aa3b907878debecd
JiYeon Ahn <76156034+jiyeonahn@users.noreply.github.com>
2024-09-06 10:38:35 +0900

    [level 1] Title: 2016년, Time: 0.04 ms, Memory: 76.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/12901. 2016년/2016년.java
@@ -0,0 +1,13 @@
+class Solution {
+    public String solution(int a, int b) {
+        String answer = "";
+        String[] day = {"THU", "FRI", "SAT", "SUN", "MON", "TUE", "WED"};
+        int[] month = {31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+        int c = b;
+        for(int i = 0; i < a-1; i++){
+            c += month[i];
+        }
+        
+        return day[c%7];
+    }
+}
