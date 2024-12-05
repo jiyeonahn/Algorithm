9dcbf6e3576b07ab686ef513aa5b2ecfc8b49577
JiYeon Ahn <76156034+jiyeonahn@users.noreply.github.com>
2024-12-05 22:46:34 +0900

    [level 0] Title: 특이한 정렬, Time: 1.34 ms, Memory: 94.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120880. 특이한 정렬/특이한 정렬.java
@@ -0,0 +1,32 @@
+import java.util.PriorityQueue;
+class Solution {
+    public int[] solution(int[] numlist, int n) {
+        int[] answer = new int[numlist.length];
+        int[] diff = new int[numlist.length];
+        for(int i = 0; i < numlist.length; i++){
+            diff[i] = numlist[i] - n;
+        }
+        
+        PriorityQueue<Integer> pq = new PriorityQueue<>((a,b) -> {
+            int absA = Math.abs(a);
+            int absB = Math.abs(b);
+            
+            if(absA != absB){
+                return absA - absB;
+            }else{
+                return a > b ? -1 : 1;
+            }
+        });
+        
+        for(int num : diff){
+            pq.add(num);
+        }
+        
+        int idx = 0;
+        while(!pq.isEmpty()){
+            answer[idx++] = pq.poll() + n;
+        }
+        
+        return answer;
+    }
+}
