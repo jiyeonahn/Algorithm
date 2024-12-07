de4205429a9d594f30a9d6e1ff4cb34348d0d83e
JiYeon Ahn <76156034+jiyeonahn@users.noreply.github.com>
2024-12-07 17:03:36 +0900

    [level 2] Title: 기능개발, Time: 0.44 ms, Memory: 72.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -0,0 +1,30 @@
+import java.util.*;
+class Solution {
+    public int[] solution(int[] progresses, int[] speeds) {
+        Queue<Integer> q = new LinkedList<>();
+        for(int i = 0; i < speeds.length; i++){
+            q.add((int) Math.ceil((double)(100 - progresses[i]) / speeds[i]));
+        }
+        q.add(100);
+        
+        Queue<Integer> ans = new LinkedList<>();
+        int n = q.peek();
+        int cnt = 0;
+        while(!q.isEmpty()){
+            if(q.peek() <= n){
+                cnt++;
+                q.poll();
+            }else{
+                ans.add(cnt);
+                cnt = 1;
+                n = q.poll();
+            }
+        }
+        
+        int[] answer = new int[ans.size()];
+        for(int i = 0; i < answer.length; i++){
+            answer[i] = ans.poll();
+        }
+        return answer;
+    }
+}
