1d522887d937b760563d11646c65e49aaa40125d
JiYeon Ahn <76156034+jiyeonahn@users.noreply.github.com>
2024-12-07 15:50:05 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 32.11 ms, Memory: 114 MB -BaekjoonHub

---
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class Solution {
+    public int[] solution(int []arr) {
+        Queue<Integer> queue = new LinkedList<>();
+        int num = -1;
+        for(int i = 0; i < arr.length; i++){
+            if(num != arr[i]){
+                queue.add(arr[i]);
+            }
+            num = arr[i];
+        }
+        int[] answer = new int[queue.size()];
+        
+        int idx = 0;
+        while(!queue.isEmpty()){
+            answer[idx++] = queue.poll();
+        }
+
+        return answer;
+    }
+}
