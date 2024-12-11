99f11aa79c44cd6f4896f18843646b03646cf253
JiYeon Ahn <76156034+jiyeonahn@users.noreply.github.com>
2024-12-11 14:02:39 +0900

    [level 1] Title: K번째수, Time: 0.46 ms, Memory: 84.9 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.java
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -2,11 +2,16 @@ import java.util.*;
 class Solution {
     public int[] solution(int[] array, int[][] commands) {
         int[] answer = new int[commands.length];
-        
         for(int i = 0; i < commands.length; i++){
-            int[] numArr = Arrays.copyOfRange(array, commands[i][0]-1, commands[i][1]);
-            Arrays.sort(numArr);
-            answer[i] = numArr[commands[i][2]-1];
+            int startIndex = commands[i][0]-1;
+            int endIndex = commands[i][1]-1;
+            int[] splitArray = new int[endIndex - startIndex + 1];
+            int index = 0;
+            for(int j = startIndex; j <= endIndex; j++){
+                splitArray[index++] = array[j];
+            }
+            Arrays.sort(splitArray);
+            answer[i] = splitArray[commands[i][2]-1];
         }
         return answer;
     }
