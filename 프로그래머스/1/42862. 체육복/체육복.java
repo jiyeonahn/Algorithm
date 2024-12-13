dba7443eb1b7786fa684dd12a8a81a06cff618ab
JiYeon Ahn <76156034+jiyeonahn@users.noreply.github.com>
2024-12-13 14:23:29 +0900

    [level 1] Title: 체육복, Time: 0.51 ms, Memory: 79.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/42862. 체육복/체육복.java
@@ -0,0 +1,29 @@
+import java.util.*;
+class Solution {
+    public int solution(int n, int[] lost, int[] reserve) {
+        Arrays.sort(lost);
+        Arrays.sort(reserve);
+        
+        int answer = n - lost.length;
+        int idx = 0;
+        for(int i = 0; i < reserve.length; i++){
+            for(int j = 0; j < lost.length; j++){
+                if(reserve[i] == lost[j]){
+                    reserve[i] = -1;
+                    lost[j] = -1;
+                    answer++;
+                }
+            }
+        }
+        for(int i = 0; i < reserve.length; i++){
+            for(int j = 0; j < lost.length; j++){
+                if(reserve[i] > 0 && lost[j] > 0 && (reserve[i] + 1 == lost[j] || reserve[i] - 1 == lost[j])){
+                    lost[j] = -1;   
+                    reserve[i] = -1;
+                    answer++;
+                }
+            }
+        }
+        return answer;
+    }
+}
