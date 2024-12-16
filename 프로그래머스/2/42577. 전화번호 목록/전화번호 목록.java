da6ad211a903aa70f7c7f9892f7a4277b63908e4
JiYeon Ahn <76156034+jiyeonahn@users.noreply.github.com>
2024-12-16 12:26:50 +0900

    [level 2] Title: 전화번호 목록, Time: 299.35 ms, Memory: 224 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -9,10 +9,9 @@ class Solution {
         
         for(int i = 0; i < phone_book.length; i++){
             String phone_num = phone_book[i];
-            for(int j = 0; j < phone_num.length()+1; j++){
+            for(int j = 0; j < phone_num.length(); j++){
                 if(hashMap.get(phone_num.substring(0,j)) != null){
-                    hashMap.put(phone_num, hashMap.get(phone_num.substring(0,j)) + 1);
-                    if(hashMap.get(phone_num.substring(0,j)) > 1){
+                    if(hashMap.containsKey(phone_num.substring(0,j))){
                         return false;
                     }
                 }
