e089f0e59d89c7c299691cb2b6653529f6a56cb7
JiYeon Ahn <76156034+jiyeonahn@users.noreply.github.com>
2024-12-20 15:03:06 +0900

    [level 2] Title: 가장 큰 수, Time: 2939.54 ms, Memory: 392 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
@@ -4,9 +4,20 @@ class Solution {
         String answer = "";
         String[] nums = new String[numbers.length];
         for(int i = 0; i < numbers.length; i++){
-            nums[i] = numbers[i]+ "";
+            nums[i] = String.valueOf(numbers[i]);
         }
         Arrays.sort(nums, (a,b) -> (b+a).compareTo(a+b));
+        // for(int i = 0; i < numbers.length; i++){
+        //     for(int j = i+1; j < numbers.length; j++){
+        //         String temp = "";
+        //         if((nums[i] + nums[j]).compareTo(nums[j] + nums[i]) < 0){
+        //             temp = nums[i];
+        //             nums[i] = nums[j];
+        //             nums[j] = temp;
+        //         }
+        //     }
+        // }
+        
         for(String num : nums){
             answer += num;
         }
