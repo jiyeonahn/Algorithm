cfb7f2c3c40b637856e2549c1c940d50d66ff585
JiYeon Ahn <76156034+jiyeonahn@users.noreply.github.com>
2024-12-20 15:11:26 +0900

    [level 2] Title: 가장 큰 수, Time: 2934.75 ms, Memory: 403 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/README.md
+++ 프로그래머스/2/42746. 가장 큰 수/README.md
@@ -1,10 +1,10 @@
 # [level 2] 가장 큰 수 - 42746 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42746) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42746?language=java) 
 
 ### 성능 요약
 
-메모리: 392 MB, 시간: 2939.54 ms
+메모리: 403 MB, 시간: 2934.75 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 12월 20일 15:03:05
+2024년 12월 20일 15:11:25
 
 ### 문제 설명
 
--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
@@ -7,16 +7,6 @@ class Solution {
             nums[i] = String.valueOf(numbers[i]);
         }
         Arrays.sort(nums, (a,b) -> (b+a).compareTo(a+b));
-        // for(int i = 0; i < numbers.length; i++){
-        //     for(int j = i+1; j < numbers.length; j++){
-        //         String temp = "";
-        //         if((nums[i] + nums[j]).compareTo(nums[j] + nums[i]) < 0){
-        //             temp = nums[i];
-        //             nums[i] = nums[j];
-        //             nums[j] = temp;
-        //         }
-        //     }
-        // }
         
         for(String num : nums){
             answer += num;
