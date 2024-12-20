3cedf2239181544652d5c4608d5eea67cb77c912
JiYeon Ahn <76156034+jiyeonahn@users.noreply.github.com>
2024-12-20 14:48:00 +0900

    [level 2] Title: 가장 큰 수, Time: 3117.12 ms, Memory: 404 MB -BaekjoonHub

---
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
@@ -0,0 +1,20 @@
+import java.util.*;
+class Solution {
+    public String solution(int[] numbers) {
+        String answer = "";
+        String[] nums = new String[numbers.length];
+        for(int i = 0; i < numbers.length; i++){
+            nums[i] = numbers[i]+ "";
+        }
+        Arrays.sort(nums, (a,b) -> (b+a).compareTo(a+b));
+        for(String num : nums){
+            answer += num;
+        }
+        
+        if(answer.charAt(0) == '0'){
+            return "0";
+        }
+        
+        return answer;
+    }
+}
