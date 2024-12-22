e76a48980fd9603fb21eb45c49bc69ea1cbc4a61
JiYeon Ahn <76156034+jiyeonahn@users.noreply.github.com>
2024-12-22 16:04:46 +0900

    [level 2] Title: 피로도, Time: 3.18 ms, Memory: 82.6 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.java
+++ 프로그래머스/2/87946. 피로도/피로도.java
@@ -1,24 +1,20 @@
 class Solution {
     int answer;
     public int solution(int k, int[][] dungeons) {
-        for(int i = 0; i < dungeons.length; i++){
-            boolean[] visited = new boolean[dungeons.length];
-            if(dungeons[i][0] <= k){
-                dfs(i, k - dungeons[i][1], dungeons, 1, visited);
-            }
-        }
+        boolean[] visited = new boolean[dungeons.length];
+        dfs(k, dungeons, 0, visited);
         
         return answer;
     }
-    public void dfs(int curIndex, int current, int[][] dungeons, int result, boolean[] visited){
+    public void dfs(int current, int[][] dungeons, int result, boolean[] visited){
         answer = Math.max(answer, result);
-        visited[curIndex] = true;
         for(int i = 0; i < dungeons.length; i++){
             if(!visited[i] && dungeons[i][0] <= current){
-                dfs(i, current - dungeons[i][1], dungeons, result+1, visited);
+                visited[i] = true;
+                dfs(current - dungeons[i][1], dungeons, result+1, visited);
+                visited[i] = false;
             }
             
         }
-        visited[curIndex] = false; //백트래킹
     }
 }
