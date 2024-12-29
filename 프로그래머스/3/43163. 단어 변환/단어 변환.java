9a167e681746352d465a6b6b0c5441c6a4003696
JiYeon Ahn <76156034+jiyeonahn@users.noreply.github.com>
2024-12-29 15:42:47 +0900

    [level 3] Title: 단어 변환, Time: 0.51 ms, Memory: 91 MB -BaekjoonHub

---
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.java
@@ -0,0 +1,32 @@
+class Solution {
+    boolean[] visited;
+    int answer;
+    public int solution(String begin, String target, String[] words) {
+        answer = 0;
+        visited = new boolean[words.length];
+        dfs(begin, 0, words, target);            
+        return answer;
+    }
+    public void dfs(String currentWord, int currentCount, String[] words, String target){
+        if(currentWord.equals(target)){
+            answer = answer == 0 ? currentCount : Math.min(answer, currentCount);
+            return;
+        }
+        
+        for(int i = 0; i < words.length; i++){
+            int equalCount = 0;
+            for(int j = 0; j < currentWord.length(); j++){
+                if(!visited[i] && currentWord.charAt(j) == words[i].charAt(j)){
+                    equalCount++;
+                }
+            }
+            if(equalCount == currentWord.length() - 1){
+                visited[i] = true;
+                dfs(words[i], currentCount + 1, words, target);
+                visited[i] = false;
+            }
+        }
+        
+        
+    }
+}
