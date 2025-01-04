e1d2fa8e9ea767fd7ce15ea0dc87af63ca285a64
JiYeon Ahn <76156034+jiyeonahn@users.noreply.github.com>
2025-01-04 23:42:20 +0900

    [Bronze III] Title: 그대로 출력하기 2, Time: 108 ms, Memory: 14212 KB -BaekjoonHub

---
+++ 백준/Bronze/11719. 그대로 출력하기 2/그대로 출력하기 2.java
@@ -0,0 +1,11 @@
+import java.util.*;
+import java.io.*;
+class Main{
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str;
+        while((str = br.readLine()) != null){
+            System.out.println(str);
+        }
+    }
+}
