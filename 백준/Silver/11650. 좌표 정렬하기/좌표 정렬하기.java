481d5cdd8f6ace4a7a2f83d2f910545478f41599
JiYeon Ahn <76156034+jiyeonahn@users.noreply.github.com>
2025-01-10 13:57:44 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 1204 ms, Memory: 56300 KB -BaekjoonHub

---
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.java
@@ -0,0 +1,26 @@
+import java.util.*;
+import java.io.*;
+class Main{
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st;
+        int[][] arr = new int[N][2];
+        for(int i = 0; i < N; i++){
+            st = new StringTokenizer(br.readLine(), " ");
+            arr[i][0] = Integer.parseInt(st.nextToken());
+            arr[i][1] = Integer.parseInt(st.nextToken());
+        }
+        
+        Arrays.sort(arr, (a,b) -> {
+            if(a[0] == b[0]){
+                return a[1] - b[1];
+            }
+            return a[0] - b[0];
+        });
+        
+        for(int i = 0; i < N; i++){
+            System.out.println(arr[i][0] + " " + arr[i][1]);
+        }
+    }
+}
