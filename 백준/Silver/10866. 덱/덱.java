60befd4b3f023cb25a173b520fb06e117818a457
JiYeon Ahn <76156034+jiyeonahn@users.noreply.github.com>
2025-01-27 11:16:46 +0900

    [Silver IV] Title: 덱, Time: 172 ms, Memory: 18960 KB -BaekjoonHub

---
+++ 백준/Silver/10866. 덱/덱.java
@@ -0,0 +1,44 @@
+import java.util.*;
+import java.io.*;
+class Main{
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        Deque<Integer> deque = new LinkedList<>();
+        StringTokenizer st;
+        int X;
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i < N; i++){
+            st = new StringTokenizer(br.readLine());
+            switch(st.nextToken()){
+                case "push_front":
+                    X = Integer.parseInt(st.nextToken());
+                    deque.addFirst(X);
+                    break;
+                case "push_back":
+                    X = Integer.parseInt(st.nextToken());
+                    deque.addLast(X);
+                    break;
+                case "pop_front":
+                    sb.append(deque.isEmpty() ? -1 : deque.pollFirst()).append("\n");
+                    break;
+                case "pop_back":
+                    sb.append(deque.isEmpty() ? -1 : deque.pollLast()).append("\n");
+                    break;
+                case "size":
+                    sb.append(deque.size()).append("\n");
+                    break;
+                case "empty":
+                    sb.append(deque.isEmpty() ? 1 : 0).append("\n");
+                    break;
+                case "front":
+                    sb.append(deque.isEmpty() ? -1 : deque.peekFirst()).append("\n");
+                    break;
+                case "back":
+                    sb.append(deque.isEmpty() ? -1 : deque.peekLast()).append("\n");
+                    break;
+            }
+        }
+        System.out.println(sb);
+    }
+}
