0e4b45fa54c9ceac80813ef2505c6219dc616493
JiYeon Ahn <76156034+jiyeonahn@users.noreply.github.com>
2025-02-07 17:29:38 +0900

    [Silver II] Title: 외판원 순회 2, Time: 132 ms, Memory: 14592 KB -BaekjoonHub

--- 백준/Silver/10971. 외판원 순회 2/외판원 순회 2.java
+++ 백준/Silver/10971. 외판원 순회 2/외판원 순회 2.java
@@ -1,44 +1,46 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.*;
+import java.io.*;
 
-public class Main {//외판원 순회 2
-    private static int N, min = 10000000;
-    private static int[][] W;
-    private static boolean[] visited;
+class Main {
+    static int N;
+    static int[][] W;
+    static boolean[] visited;
+    static int answer = Integer.MAX_VALUE;
 
-    public static void dfs(int idx, int sum, int len){
-        if(len == N && W[idx][0] != 0){
-            min = Math.min(min, sum + W[idx][0]);
+    public static void dfs(int current, int sum, int len) {
+        if (len == N-1) {
+            if (W[current][0] != 0) {
+                answer = Math.min(answer, sum + W[current][0]);
+            }
             return;
         }
-
-        for(int j = 0; j < N; j++){
-            if(W[idx][j] != 0 && !visited[j]){
-                visited[j] = true;
-                dfs(j,sum + W[idx][j], len+1);
-                visited[j] = false;
+        for (int i = 0; i < N; i++) {
+            if (W[current][i] != 0 && !visited[i]) {
+                visited[i] = true;
+                dfs(i, sum + W[current][i], len + 1);
+                visited[i] = false;
             }
         }
     }
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
         N = Integer.parseInt(br.readLine());
         W = new int[N][N];
         visited = new boolean[N];
 
-        StringTokenizer st;
-        for(int i = 0; i < N; i++){
-            st = new StringTokenizer(br.readLine()," ");
-            for(int j = 0; j < N; j++){
-                W[i][j] =Integer.parseInt(st.nextToken());
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < N; j++) {
+                W[i][j] = Integer.parseInt(st.nextToken());
             }
         }
-        visited[0] = true;
-        dfs(0,0,1);
 
-        System.out.println(min);
+        //출발지점 0으로 고정
+        visited[0] = true;
+        dfs(0, 0, 0);//현재지점, 합, 길이
+
+        System.out.println(answer);
     }
 }
