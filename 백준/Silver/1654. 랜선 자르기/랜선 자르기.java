43da6ec73a1f442ba53a42698d049afac2226a2c
JiYeon Ahn <76156034+jiyeonahn@users.noreply.github.com>
2025-02-10 16:21:41 +0900

    [Silver II] Title: 랜선 자르기, Time: 164 ms, Memory: 17652 KB -BaekjoonHub

--- 백준/Silver/1654. 랜선 자르기/랜선 자르기.java
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.java
@@ -1,12 +1,9 @@
 import java.util.*;
 import java.io.*;
-
 class Main{
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-
+        StringTokenizer st = new StringTokenizer(br.readLine());
         int K = Integer.parseInt(st.nextToken());
         int N = Integer.parseInt(st.nextToken());
 
@@ -16,32 +13,27 @@ class Main{
             arr[i] = Integer.parseInt(br.readLine());
         }
 
-        long max = 0;
+        Arrays.sort(arr);
 
-        for(int num : arr){
-            max = Math.max(num, max);
-        }
+        long low = 1; //mid가 0이 되면 안 되므로
+        long high = arr[K-1];
 
-        long low = 1;
-        long high = max;
-        long result = 0;
+        long answer = 0;
 
         while(low <= high){
-            long mid = (low + high) / 2;
-
-            long count = 0;
-
+            long mid = (low+high)/2;
+            long cnt = 0;
             for(int i = 0; i < K; i++){
-                count += arr[i] / mid;
+                cnt += arr[i]/mid;
             }
 
-            if(count >= N){
-                result = mid;
-                low = mid + 1;//랜선의 최대 길이를 찾아야하므로
+            if(cnt >= N){ // 현재 mid 길이로 N개 이상을 만들 수 있음, 현재 길이보다 더 긴 길이로 만들 수 있음
+                low = mid+1; // 더 긴 길이 시도
+                answer = mid;
             }else{
-                high = mid - 1;
+                high = mid-1; // 더 짧은 길이 시도
             }
         }
-        System.out.println(result);
+        System.out.println(answer);
     }
 }
