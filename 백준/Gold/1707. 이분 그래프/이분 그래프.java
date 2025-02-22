a2211de4bdeb7686c276adb3ae73b3bffd22c54d
안지연 <76156034+jiyeonahn@users.noreply.github.com>
2025-02-22 20:35:20 +0900

    [Gold IV] Title: 이분 그래프, Time: 836 ms, Memory: 271916 KB -BaekjoonHub

--- 백준/Gold/1707. 이분 그래프/이분 그래프.java
+++ 백준/Gold/1707. 이분 그래프/이분 그래프.java
@@ -1,59 +1,69 @@
 import java.util.*;
 import java.io.*;
 class Main{
-    static StringBuilder answer = new StringBuilder();
+    static int[] visited;
+    static int V;
+    static ArrayList<Integer>[] graph;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int K = Integer.parseInt(br.readLine());
-        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+
         for(int i = 0; i < K; i++){
-            st = new StringTokenizer(br.readLine());
-            int V = Integer.parseInt(st.nextToken());
-            int E = Integer.parseInt(st.nextToken());
-            //ArrayList를 요소로 갖는 배열
-            ArrayList<Integer>[] graph = new ArrayList[V];
-            for(int j = 0; j < V; j++){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            V = Integer.parseInt(st.nextToken());//정점의 개수
+            int E = Integer.parseInt(st.nextToken());//간선의 개수
+
+            graph = new ArrayList[V+1];
+
+            for(int j = 0; j < V+1; j++){
                 graph[j] = new ArrayList<>();
             }
+
             for(int j = 0; j < E; j++){
                 st = new StringTokenizer(br.readLine());
-                int node1 = Integer.parseInt(st.nextToken()) - 1;
-                int node2 = Integer.parseInt(st.nextToken()) - 1;
-
-                graph[node1].add(node2);
-                graph[node2].add(node1);
+                int a = Integer.parseInt(st.nextToken());
+                int b = Integer.parseInt(st.nextToken());
+                graph[a].add(b);
+                graph[b].add(a);
             }
-            int[] colors = new int[V];
-            boolean result = true;
-            for(int j = 0; j < V; j++){
-                if(colors[j] == 0){
-                    if(!bfs(j, graph, colors)){
-                        result = false;
+
+            boolean isBipartiteGraph = true;
+
+            visited = new int[V+1];//-1은 왼쪽, 1은 오른쪽
+
+            for(int j = 1; j < V+1; j++){
+                if(visited[j] == 0){
+                    if(!isBipartiteGraph(j)){
+                        isBipartiteGraph = false;
                         break;
                     }
                 }
             }
-            answer.append(result ? "YES" : "NO").append("\n");
+
+            sb.append(isBipartiteGraph ? "YES" : "NO").append("\n");
         }
-        System.out.println(answer);
+        System.out.println(sb);
     }
-    public static boolean bfs(int n, ArrayList<Integer>[] graph, int[] colors){
+    public static boolean isBipartiteGraph(int n){
         Queue<Integer> queue = new LinkedList<>();
+        
         queue.add(n);
-        colors[n] = 1;
+        visited[n] = -1;
 
         while(!queue.isEmpty()){
-            int current = queue.poll();
-            for(int g : graph[current]){
-                if(colors[g] == colors[current]){
+            int m = queue.poll();
+            for(int num : graph[m]){
+                if(visited[num] == visited[m]){
                     return false;
                 }
-                if(colors[g] == 0){
-                    colors[g] = -colors[current];
-                    queue.add(g);
+                if(visited[num] == 0){
+                    queue.add(num);
+                    visited[num] = -visited[m];
                 }
             }
         }
+
         return true;
     }
 }
