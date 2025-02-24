c2dba04ec1e3773520365764ee67ad59960a375b
안지연 <76156034+jiyeonahn@users.noreply.github.com>
2025-02-24 16:13:43 +0900

    [Silver I] Title: 단지번호붙이기, Time: 100 ms, Memory: 14204 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
@@ -1,53 +1,57 @@
 import java.io.*;
-import java.util.PriorityQueue;
+import java.util.*;
 
 class Main{
-    static int count;
+    static int[][] map;
+    static boolean[][] visited;
+    static int[] dx = {0,1,0,-1};
+    static int[] dy = {1,0,-1,0};
+    static int N, homeInComplex;
+    static ArrayList<Integer> home = new ArrayList<>();
     public static void main(String[] args) throws IOException {
+        StringBuilder sb = new StringBuilder();
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
-        int[][] map = new int[N][N];
+        N = Integer.parseInt(br.readLine());
+        map = new int[N][N];
+        visited = new boolean[N][N];
         for(int i = 0; i < N; i++){
             String str = br.readLine();
             for(int j = 0; j < N; j++){
-                map[i][j] = str.charAt(j) - '0';
+                map[i][j] = str.charAt(j)-'0';
             }
         }
-        int total = 0;
-        StringBuilder sb = new StringBuilder();
-        PriorityQueue<Integer> pq = new PriorityQueue<>();
+
+        int complex = 0;
         for(int i = 0; i < N; i++){
             for(int j = 0; j < N; j++){
-                if(map[i][j] > 0){
-                    count = 1;
-                    map[i][j] = 0;
-                    total++;
-                    dfs(map, i, j, N);
-                    pq.add(count);
+                if(!visited[i][j] && map[i][j] == 1){
+                    homeInComplex = 0;
+                    complex++;
+                    dfs(i,j);
+                    home.add(homeInComplex);
                 }
             }
         }
-        sb.append(total);
-        while(!pq.isEmpty()){
-            sb.append("\n").append(pq.poll());
+        Collections.sort(home);
+        sb.append(complex).append("\n");
+        for(int h : home){
+            sb.append(h).append("\n");
         }
         System.out.println(sb);
     }
-    public static void dfs(int[][] map, int x, int y, int N){
-        int[] dx = {-1, 0, 1, 0};
-        int[] dy = {0, -1, 0, 1};
-
+    public static void dfs(int x, int y){
+        visited[x][y] = true;
+        homeInComplex++;
         for(int i = 0; i < 4; i++){
             int dx_x = x + dx[i];
             int dy_y = y + dy[i];
 
             if(dx_x >= 0 && dx_x < N && dy_y >= 0 && dy_y < N){
-                if(map[dx_x][dy_y] > 0){
-                    count++;
-                    map[dx_x][dy_y] = 0;
-                    dfs(map, dx_x, dy_y, N);
+                if(!visited[dx_x][dy_y] && map[dx_x][dy_y] == 1){
+                    dfs(dx_x, dy_y);
                 }
             }
         }
+
     }
 }
