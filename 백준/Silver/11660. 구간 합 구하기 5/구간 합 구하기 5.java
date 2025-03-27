11cb42614bffffbbdeebf5531191195db21141c1
안지연 <76156034+jiyeonahn@users.noreply.github.com>
2025-03-27 20:57:51 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 744 ms, Memory: 122744 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
@@ -8,23 +8,18 @@ class Main {
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
 
-        int[][] arr = new int[N][N];
+        int[][] arr = new int[N+1][N+1];
 
-        for (int i = 0; i < N; i++) {
+        for (int i = 1; i <= N; i++) {
             st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < N; j++) {
+            for (int j = 1; j <= N; j++) {
                 arr[i][j] = Integer.parseInt(st.nextToken());
             }
         }
 
-        int[][] dp = new int[N][N];
-        dp[0][0] = arr[0][0];
-        for(int i = 1; i < N; i++){
-            dp[i][0] = dp[i-1][0] + arr[i][0];
-            dp[0][i] = dp[0][i - 1] + arr[0][i];
-        }
-        for (int i = 1; i < N; i++) {
-            for (int j = 1; j < N; j++) {
+        int[][] dp = new int[N+1][N+1];
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= N; j++) {
                 dp[i][j] = arr[i][j] + dp[i - 1][j] + dp[i][j - 1] - dp[i - 1][j - 1];
             }
         }
@@ -34,23 +29,12 @@ class Main {
         for (int i = 0; i < M; i++) {
             int sum = 0;
             st = new StringTokenizer(br.readLine());
-            int x1 = Integer.parseInt(st.nextToken()) - 1;
-            int y1 = Integer.parseInt(st.nextToken()) - 1;
-            int x2 = Integer.parseInt(st.nextToken()) - 1;
-            int y2 = Integer.parseInt(st.nextToken()) - 1;
+            int x1 = Integer.parseInt(st.nextToken());
+            int y1 = Integer.parseInt(st.nextToken());
+            int x2 = Integer.parseInt(st.nextToken());
+            int y2 = Integer.parseInt(st.nextToken());
 
-            int condition = 0;
-            if(y1 > 0){
-                condition += dp[x2][y1 - 1];
-            }
-            if(x1 > 0){
-                condition += dp[x1 - 1][y2];
-            }
-            if(x1 > 0 && y1 > 0){
-                condition -= dp[x1 - 1][y1 - 1];
-            }
-
-            sum = dp[x2][y2] - (condition);
+            sum = dp[x2][y2] - (dp[x1 - 1][y2] + dp[x2][y1 - 1] - dp[x1 - 1][y1 - 1]);
 
             sb.append(sum).append("\n");
         }
