6db7f1d6bcbcf32b99c7d02d8b8f6b42fe592932
안지연 <76156034+jiyeonahn@users.noreply.github.com>
2025-04-04 17:21:15 +0900

    [Silver II] Title: N과 M (9), Time: 172 ms, Memory: 20784 KB -BaekjoonHub

--- 백준/Silver/15663. N과 M （9）/N과 M （9）.java
+++ 백준/Silver/15663. N과 M （9）/N과 M （9）.java
@@ -1,50 +1,47 @@
 import java.util.*;
 import java.io.*;
-
-class Main {
-    static int[] arr;
-    static int N, M;
+class Main{
     static boolean[] visited;
     static StringBuilder sb = new StringBuilder();
-    static int[] sequence;
-
-    public static void main(String[] args) throws IOException {
+    static int N,M;
+    static int[] arr, sequence;
+    public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
+
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
 
-        st = new StringTokenizer(br.readLine());
         arr = new int[N];
+        sequence = new int[M];
 
-        for (int i = 0; i < N; i++) {
+        st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < arr.length; i++){
             arr[i] = Integer.parseInt(st.nextToken());
         }
 
+        visited = new boolean[N];
         Arrays.sort(arr);
 
-        visited = new boolean[N];
-        sequence = new int[M];
         dfs(0);
 
         System.out.println(sb);
     }
-
-    public static void dfs(int idx) {
-        if (idx == M) {
-            for (int s : sequence) {
+    public static void dfs(int idx){
+        if(idx == M){
+            for(int s : sequence){
                 sb.append(s).append(" ");
             }
             sb.append("\n");
             return;
         }
         int before = 0;
-        for (int i = 0; i < N; i++) {
-            if (!visited[i] && before != arr[i]) {
+        for(int i = 0; i < arr.length; i++){
+            if(!visited[i] && before != arr[i]){
                 visited[i] = true;
                 before = arr[i];
                 sequence[idx] = arr[i];
-                dfs(idx + 1);
+                dfs(idx+1);
                 visited[i] = false;
             }
         }
