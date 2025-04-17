9591cde3824c2bfc7ce6640f68b30ae43181ed5e
안지연 <76156034+jiyeonahn@users.noreply.github.com>
2025-04-17 21:39:18 +0900

    [Silver III] Title: 진우의 달 여행 (Small), Time: 108 ms, Memory: 14296 KB -BaekjoonHub

---
+++ 백준/Silver/17484. 진우의 달 여행 （Small）/진우의 달 여행 （Small）.java
@@ -0,0 +1,47 @@
+import java.util.*;
+import java.io.*;
+class Main{
+    static int min = Integer.MAX_VALUE;
+    static int N,M;
+    static int[][] arr;
+    static int[] dx = {1,1,1};
+    static int[] dy = {-1,0,1};
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        arr = new int[N][M];
+
+        for(int i = 0; i < N; i++){
+            st = new StringTokenizer(br.readLine());
+            for(int j = 0; j < M; j++){
+                arr[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        for(int i = 0; i < M; i++){
+            dfs(0,i, -1, arr[0][i]);
+        }
+
+        System.out.println(min);
+    }
+    public static void dfs(int x, int y, int dir, int sum){
+        if(x == N-1){
+            min = Math.min(min, sum);
+        }
+
+        for(int i = 0; i < 3; i++){
+            if(i != dir){
+                int nx = x+dx[i];
+                int ny = y+dy[i];
+
+                if(nx < N && ny >= 0 && ny < M){
+                    dfs(nx,ny,i,sum+arr[nx][ny]);
+                }
+            }
+        }
+    }
+}
