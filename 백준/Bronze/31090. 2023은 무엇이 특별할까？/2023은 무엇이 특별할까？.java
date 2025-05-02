bdb1189933107a87d789d2187c2ac50d98db6260
안지연 <76156034+jiyeonahn@users.noreply.github.com>
2025-05-02 11:27:09 +0900

    [Bronze IV] Title: 2023은 무엇이 특별할까?, Time: 128 ms, Memory: 14872 KB -BaekjoonHub

--- 백준/Bronze/31090. 2023은 무엇이 특별할까？/2023은 무엇이 특별할까？.java
+++ 백준/Bronze/31090. 2023은 무엇이 특별할까？/2023은 무엇이 특별할까？.java
@@ -1,18 +1,23 @@
 import java.io.*;
-class Main{
+
+class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int T = Integer.parseInt(br.readLine());
 
         StringBuilder sb = new StringBuilder();
-        for(int i = 0; i < T; i++){
-            String N = br.readLine();
-            int endN = Integer.parseInt(N.substring(2));
-            if((Integer.parseInt(N)+1)%endN == 0){
-                sb.append("Good");
-            }else{
+        for (int i = 0; i < T; i++) {
+            int N = Integer.parseInt(br.readLine());
+            if (N % 100 == 0) {
                 sb.append("Bye");
+            } else {
+                if ((N + 1) % (N % 100) == 0) {
+                    sb.append("Good");
+                } else {
+                    sb.append("Bye");
+                }
             }
+
             sb.append("\n");
         }
         System.out.println(sb);
