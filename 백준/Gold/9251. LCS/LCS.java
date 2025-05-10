5d38ddecea5d526ef6f11a8c53a964b1ae36ad7c
안지연 <76156034+jiyeonahn@users.noreply.github.com>
2025-05-10 13:37:18 +0900

    [Gold V] Title: LCS, Time: 140 ms, Memory: 19436 KB -BaekjoonHub

--- 백준/Gold/9251. LCS/LCS.java
+++ 백준/Gold/9251. LCS/LCS.java
@@ -1,40 +1,48 @@
 import java.util.*;
 import java.io.*;
-class Main{
-    public static void main(String[] args) throws IOException{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String str1 = br.readLine();
         String str2 = br.readLine();
 
-        int[][] dp = new int[str1.length()][str2.length()];
+        int len1 = str1.length();
+        int len2 = str2.length();
 
-        int max = 0;
-        for(int i = 0; i < str2.length(); i++){
-            if(str1.charAt(0) == str2.charAt(i)){
-                max = 1;
+        int[][] dp = new int[len1][len2];
+
+        for (int i = 0; i < len1; i++) {
+            if (str1.charAt(i) == str2.charAt(0)) {
+                dp[i][0] = 1;
+                continue;
             }
-            dp[0][i] = max;
+            if (i > 0) {
+                dp[i][0] = dp[i - 1][0];
+            }
         }
 
-        max = 0;
-        for(int i = 0; i < str1.length(); i++){
-            if(str2.charAt(0) == str1.charAt(i)){
-                max = 1;
+        for (int i = 0; i < len2; i++) {
+            if (str2.charAt(i) == str1.charAt(0)) {
+                dp[0][i] = 1;
+                continue;
             }
-            dp[i][0] = max;
+            if (i > 0) {
+                dp[0][i] = dp[0][i - 1];
+            }
         }
 
-        for(int i = 1; i < str1.length(); i++){
-            for(int j = 1; j < str2.length(); j++){
-                if(str1.charAt(i) == str2.charAt(j)){
+        for (int i = 1; i < len1; i++) {
+            for (int j = 1; j < len2; j++) {
+                if (str1.charAt(i) == str2.charAt(j)) {
                     dp[i][j] = dp[i-1][j-1] + 1;
-                }else{
-                    dp[i][j] = Math.max(dp[i][j-1], dp[i-1][j]);
+                } else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
                 }
             }
         }
-        System.out.println(dp[str1.length()-1][str2.length()-1]);
+
+        System.out.print(dp[len1-1][len2-1]);
 
     }
 }
