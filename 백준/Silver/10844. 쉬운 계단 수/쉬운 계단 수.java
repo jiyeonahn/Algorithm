52b13888854043038ef546c511eb24111ad9c13b
안지연 <76156034+jiyeonahn@users.noreply.github.com>
2025-05-13 19:46:11 +0900

    [Silver I] Title: 쉬운 계단 수, Time: 104 ms, Memory: 14268 KB -BaekjoonHub

--- 백준/Silver/10844. 쉬운 계단 수/쉬운 계단 수.java
+++ 백준/Silver/10844. 쉬운 계단 수/쉬운 계단 수.java
@@ -1,29 +1,34 @@
+import java.util.*;
 import java.io.*;
-class Main{
-    static final int MOD = 1000000000;
+
+class Main {
+    static int MOD = 1000000000;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
         int N = Integer.parseInt(br.readLine());
-        int[][] dp = new int[N+1][10];
-        for(int i = 1; i < 10; i++){
+
+        long[][] dp = new long[N + 1][10];//[자릿수][시작수]
+
+        for (int i = 0; i < 10; i++) {
             dp[1][i] = 1;
         }
 
-        for(int i = 2; i <= N; i++){
-            for(int j = 0; j <= 9; j++){
-                if(j == 0){
-                    dp[i][j] = dp[i-1][1] % MOD; //0은 1뒤에만 올 수 있다.
-                }else if(j == 9){
-                    dp[i][j] = dp[i-1][8] % MOD;//9는 8뒤에만 올 수 있다.
-                }else{
-                    dp[i][j] = (dp[i-1][j-1] + dp[i-1][j+1]) % MOD;
-                }
+        for (int i = 2; i <= N; i++) {
+            dp[i][0] = dp[i - 1][1] % MOD;
+            for (int j = 1; j < 9; j++) {
+                dp[i][j] = (dp[i - 1][j - 1] + dp[i - 1][j + 1]) % MOD;
             }
+            dp[i][9] = dp[i - 1][8] % MOD;
         }
-        long answer = 0;
-        for(int n : dp[N]){
-            answer += n % MOD;
+
+        long sum = 0;
+
+        for (int i = 1; i < 10; i++) {
+            sum += dp[N][i];
         }
-        System.out.println(answer % MOD);
+
+        System.out.println(sum % MOD);
     }
 }
