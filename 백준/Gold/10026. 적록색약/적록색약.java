773b9de03faa6c463eb17525852c24a16055512d
안지연 <76156034+jiyeonahn@users.noreply.github.com>
2025-05-19 14:48:20 +0900

    [Gold V] Title: 적록색약, Time: 116 ms, Memory: 14792 KB -BaekjoonHub

--- 백준/Gold/10026. 적록색약/적록색약.java
+++ 백준/Gold/10026. 적록색약/적록색약.java
@@ -1,71 +1,75 @@
 import java.util.*;
 import java.io.*;
-
-class Main {
-    static int N;
-    static char[][] picture;
+class Main{
     static boolean[][] visited;
-    static int[] dx = {0, 1, 0, -1};
-    static int[] dy = {1, 0, -1, 0};
-
+    static char C;
+    static char[][] color;
+    static int N;
+    static int[] dx = {0,1,0,-1};
+    static int[] dy = {1,0,-1,0};
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(br.readLine());
-        picture = new char[N][N];
-        visited = new boolean[N][N];
-        for (int i = 0; i < N; i++) {
+
+        color = new char[N][N];
+
+        for(int i = 0; i < N; i++){
             String str = br.readLine();
-            for (int j = 0; j < N; j++) {
-                picture[i][j] = str.charAt(j);
+            color[i] = str.toCharArray();
+        }
+
+        StringBuilder sb = new StringBuilder();
+        visited = new boolean[N][N];
+        int count = 0;
+        for(int i = 0; i < N; i++){
+            for(int j = 0; j < N; j++){
+                if(!visited[i][j]){
+                    visited[i][j] = true;
+                    count++;
+                    C = color[i][j];
+                    dfs(i,j);
+                }
             }
         }
-        int color = 0;
 
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < N; j++) {
-                if (!visited[i][j]) {
-                    visited[i][j] = true;
-                    color++;
-                    dfs(i, j, picture[i][j]);
+        sb.append(count).append(" ");
+
+        for(int i = 0; i < N; i++){
+            for(int j = 0; j < N; j++){
+                if(color[i][j] == 'R'){
+                    color[i][j] = 'G';
                 }
             }
         }
 
         visited = new boolean[N][N];
-        int color_blindness = 0;
-        for(int i = 0; i < N; i++) {
+        count = 0;
+        for(int i = 0; i < N; i++){
             for(int j = 0; j < N; j++){
-                if(picture[i][j] == 'G'){
-                    picture[i][j] = 'R';
-                }
-            }
-        }
-
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < N; j++) {
-                if (!visited[i][j]) {
+                if(!visited[i][j]){
                     visited[i][j] = true;
-                    color_blindness++;
-                    dfs(i, j, picture[i][j]);
+                    count++;
+                    C = color[i][j];
+                    dfs(i,j);
                 }
             }
         }
 
-        System.out.println(color + " " + color_blindness);
+        sb.append(count);
+
+        System.out.println(sb);
     }
+    public static void dfs(int x, int y){
+        for(int i = 0; i < 4; i++){
+            int nx = x + dx[i];
+            int ny = y + dy[i];
 
-    public static void dfs(int x, int y, char ch) {
-        for (int i = 0; i < 4; i++) {
-            int dx_x = x + dx[i];
-            int dy_y = y + dy[i];
-
-            if (dx_x >= 0 && dx_x < N && dy_y >= 0 && dy_y < N) {
-                if (!visited[dx_x][dy_y] && picture[dx_x][dy_y] == ch) {
-                    visited[dx_x][dy_y] = true;
-                    dfs(dx_x, dy_y, ch);
+            if(nx >= 0 && nx < N && ny >= 0 && ny < N){
+                if(!visited[nx][ny] && color[nx][ny] == C){
+                    visited[nx][ny] = true;
+                    dfs(nx,ny);
                 }
             }
         }
     }
-
 }
