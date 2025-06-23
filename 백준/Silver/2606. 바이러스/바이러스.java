20a05edaa2eadce46cbfe43e08859cd3908f5459
안지연 <76156034+jiyeonahn@users.noreply.github.com>
2025-06-23 14:33:02 +0900

    [Silver III] Title: 바이러스, Time: 104 ms, Memory: 14088 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.java
+++ 백준/Silver/2606. 바이러스/바이러스.java
@@ -5,35 +5,41 @@ class Main{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
         int M = Integer.parseInt(br.readLine());
-        ArrayList<Integer>[] computer = new ArrayList[N+1];
-        for(int i = 0; i < N+1; i++){
-            computer[i] = new ArrayList<>();
+
+        ArrayList<Integer>[] network = new ArrayList[N+1];
+
+        for(int i = 0; i <= N; i++){
+            network[i] = new ArrayList<>();
         }
+
         for(int i = 0; i < M; i++){
             StringTokenizer st = new StringTokenizer(br.readLine());
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
 
-            computer[a].add(b);
-            computer[b].add(a);
+            network[a].add(b);
+            network[b].add(a);
         }
 
-        boolean[] visited = new boolean[N+1];
-        int count = 0;
         Queue<Integer> queue = new LinkedList<>();
         queue.add(1);
+
+        boolean[] visited = new boolean[N+1];
         visited[1] = true;
+
+        int answer = -1;
+
         while(!queue.isEmpty()){
-            int virus = queue.poll();
-            for(int v : computer[virus]){
+            int n = queue.poll();
+            answer++;
+            for(int v : network[n]){
                 if(!visited[v]){
-                    count++;
                     visited[v] = true;
                     queue.add(v);
                 }
-
             }
         }
-        System.out.println(count);
+
+        System.out.println(answer);
     }
 }
