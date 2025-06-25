37fc4e7e6f345e43183319bbe26072d08e75cd3b
안지연 <76156034+jiyeonahn@users.noreply.github.com>
2025-06-25 20:01:21 +0900

    [Silver II] Title: 촌수계산, Time: 100 ms, Memory: 14280 KB -BaekjoonHub

--- 백준/Silver/2644. 촌수계산/촌수계산.java
+++ 백준/Silver/2644. 촌수계산/촌수계산.java
@@ -1,11 +1,9 @@
 import java.util.*;
 import java.io.*;
-
-class Main {
+class Main{
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-
         StringTokenizer st = new StringTokenizer(br.readLine());
 
         int a = Integer.parseInt(st.nextToken());
@@ -13,41 +11,41 @@ class Main {
 
         int m = Integer.parseInt(br.readLine());
 
-        ArrayList<Integer>[] family = new ArrayList[n + 1];
-        for (int i = 0; i <= n; i++) {
-            family[i] = new ArrayList<>();
+        ArrayList<Integer>[] list = new ArrayList[n+1];
+        for(int i = 0; i <= n; i++){
+            list[i] = new ArrayList<>();
         }
 
-        for (int i = 0; i < m; i++) {
+        for(int i = 0; i < m; i++){
             st = new StringTokenizer(br.readLine());
             int x = Integer.parseInt(st.nextToken());
             int y = Integer.parseInt(st.nextToken());
-            family[x].add(y);
-            family[y].add(x);
+
+            list[x].add(y);
+            list[y].add(x);
         }
 
         Queue<int[]> queue = new LinkedList<>();
-        boolean[] visited = new boolean[n + 1];
-        queue.add(new int[]{a, 0});
+        queue.add(new int[] {a, 0});
+        boolean[] visited = new boolean[n+1];
         visited[a] = true;
-        int count = -1;
-        while (!queue.isEmpty()) {
-            int[] cur = queue.poll();
-            int p = cur[0];
-            int cnt = cur[1];
+        int answer = 0;
+        while(!queue.isEmpty()){
+            int[] now = queue.poll();
 
-            if (p == b) {
-                count = cnt;
+            if(now[0] == b){
+                answer = now[1];
                 break;
             }
 
-            for (int f : family[p]) {
-                if (!visited[f]) {
-                    visited[f] = true;
-                    queue.add(new int[]{f, cnt + 1});
+            for(int l : list[now[0]]){
+                if(!visited[l]){
+                    visited[l] = true;
+                    queue.add(new int[]{l, now[1]+1});
                 }
             }
         }
-        System.out.println(count);
+
+        System.out.println(answer == 0 ? -1 : answer);
     }
 }
