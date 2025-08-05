7daf99428c867eea016c56064c26d0088fcecbad
안지연 <76156034+jiyeonahn@users.noreply.github.com>
2025-08-05 21:45:36 +0900

    [Silver I] Title: 경로 찾기, Time: 168 ms, Memory: 16268 KB -BaekjoonHub

---
+++ 백준/Silver/11403. 경로 찾기/경로 찾기.java
@@ -0,0 +1,38 @@
+import java.util.*;
+import java.io.*;
+class Main{
+    static final int INF = Integer.MAX_VALUE;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        int[][] dist = new int[N][N];
+
+        for(int i = 0; i < N; i++){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for(int j = 0; j < N; j++){
+                dist[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        for(int k = 0; k < N; k++){
+            for(int i = 0; i < N; i++){
+                for(int j = 0; j < N; j++){
+                    if(dist[i][k] == 1 && dist[k][j] == 1){
+                        dist[i][j] = 1;
+                    }
+                }
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i < N; i++){
+            for(int j = 0; j < N; j++){
+                sb.append(dist[i][j]).append(" ");
+            }
+            sb.append("\n");
+        }
+
+        System.out.println(sb);
+    }
+}
